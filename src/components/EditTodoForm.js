ff2fd70a124d95ebecb5e55253608661bbbbb233
ry4nn9 <ryanng2688@gmail.com>
2023-12-23 23:24:59 -0800

    add features

---
+++ src/components/EditTodoForm.js
@@ -0,0 +1,26 @@
+import React, {useState} from 'react'
+
+export const EditTodoForm = ({editTodo, task}) => {
+    const [value, setValue] = useState(task.task)
+
+    const handleSubmit = (e) => {
+        // prevent reloading of page
+        e.preventDefault();
+
+        if (value) {
+            editTodo(value, task.id)
+
+            // reset to empty string
+            setValue("")
+        }
+    }
+
+    return (
+        <form className = 'TodoForm' onSubmit={handleSubmit}>
+            <input type="text" className='todo-input' value={value}
+            placeholder='Update Task' 
+            onChange={(e) => setValue(e.target.value)}/>
+            <button type='submit' className='todo-btn'>Update Task</button>
+        </form>
+    )
+}
---
+++ src/components/TodoForm.js
@@ -0,0 +1,26 @@
+import React, {useState} from 'react'
+
+export const TodoForm = ({addTodo}) => {
+    const [value, setValue] = useState("")
+
+    const handleSubmit = (e) => {
+        // prevent reloading of page
+        e.preventDefault();
+
+        if (value) {
+            addTodo(value)
+
+            // reset to empty string
+            setValue("")
+        }
+    }
+
+    return (
+        <form className = 'TodoForm' onSubmit={handleSubmit}>
+            <input type="text" className='todo-input' value={value}
+            placeholder='What is the task today?' 
+            onChange={(e) => setValue(e.target.value)}/>
+            <button type='submit' className='todo-btn'>Add Task</button>
+        </form>
+    )
+}
